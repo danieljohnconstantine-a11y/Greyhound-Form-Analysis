fd4c55e16c8224574b77127203cac373886de2c9
danieljohnconstantine-a11y <danieljohnconstantine@gmail.com>
2025-10-07 18:32:50 +1000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,16 @@
+import os
+import pandas as pd
+
+
+def ensure_dirs(dir_list):
+    for d in dir_list:
+        os.makedirs(d, exist_ok=True)
+
+
+def save_df(df: pd.DataFrame, path: str):
+    df.to_csv(path, index=False)
+
+
+def load_df(path: str) -> pd.DataFrame:
+    return pd.read_csv(path)
 
