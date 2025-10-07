5b494627b1e328a337aad21469f2f1f03d24eb2e
danieljohnconstantine-a11y <danieljohnconstantine@gmail.com>
2025-10-07 18:08:06 +1000

    Delete scripts directory

--- scripts/run_daily.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Example wrapper: parse PDFs for a date and generate picks
-DATE=${1:-$(date +%F)}
-echo "Processing date: $DATE"
-python3 pipeline/parse_forms.py --date "$DATE"
-python3 analysis/predict.py --date "$DATE" --top 1
-echo "Done. Picks are in out/picks_${DATE}.csv"
